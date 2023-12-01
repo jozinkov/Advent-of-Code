b72250f2c4618259823b39211a3cdf0487352643
jozinkov <vicenjj@gmail.com>
2023-12-01 17:18:48 +0100

    new changes

--- 2023/day01.js
+++ 2023/day01.js
@@ -14,13 +14,73 @@ const getResults = function (fileData) {
     'eight',
     'nine',
   ];
+
   const calibration = function (regex, letters = false) {
     return data.reduce((acc, line) => {
       const nums = line.match(regex);
-      if (letters)
+      if (letters) {
+        let last = undefined;
+        line.split('').forEach((d, i) => {
+          if (/\d/.test(d)) last = d;
+          else if (/[a-z]/.test(d)) {
+            if (d == 'o' && line[i + 1] == 'n' && line[i + 2] == 'e')
+              last = 'one';
+            if (d == 't' && line[i + 1] == 'w' && line[i + 2] == 'o')
+              last = 'two';
+            if (
+              d == 't' &&
+              line[i + 1] == 'h' &&
+              line[i + 2] == 'r' &&
+              line[i + 3] == 'e' &&
+              line[i + 4] == 'e'
+            )
+              last = 'three';
+            if (
+              d == 'f' &&
+              line[i + 1] == 'o' &&
+              line[i + 2] == 'u' &&
+              line[i + 3] == 'r'
+            )
+              last = 'four';
+            if (
+              d == 'f' &&
+              line[i + 1] == 'i' &&
+              line[i + 2] == 'v' &&
+              line[i + 3] == 'e'
+            )
+              last = 'five';
+            if (d == 's' && line[i + 1] == 'i' && line[i + 2] == 'x')
+              last = 'six';
+            if (
+              d == 's' &&
+              line[i + 1] == 'e' &&
+              line[i + 2] == 'v' &&
+              line[i + 3] == 'e' &&
+              line[i + 4] == 'n'
+            )
+              last = 'seven';
+            if (
+              d == 'e' &&
+              line[i + 1] == 'i' &&
+              line[i + 2] == 'g' &&
+              line[i + 3] == 'h' &&
+              line[i + 4] == 't'
+            )
+              last = 'eight';
+            if (
+              d == 'n' &&
+              line[i + 1] == 'i' &&
+              line[i + 2] == 'n' &&
+              line[i + 3] == 'e'
+            )
+              last = 'nine';
+          }
+        });
+        nums.push(last);
         nums.forEach(
           (n, i) => (nums[i] = n.length > 1 ? `${arr.indexOf(n) + 1}` : n)
         );
+      }
       return (acc += Number(nums[0] + nums[nums.length - 1]));
     }, 0);
   };
