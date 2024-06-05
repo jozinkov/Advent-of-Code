9df99c81cb86d5da07aff42609de1224317460a2
JozefVicen <91002232+jozinkov@users.noreply.github.com>
2024-06-05 18:36:26 +0200

    Update day12.js

--- 2023/day12.js
+++ 2023/day12.js
@@ -3,5 +3,6 @@ let res2 = 0;
 
 const getResults = function (fileData) {
   const data = fileData.split('\n').filter(d => d !== '');
+  // comment to test new git branch
 };
 export { getResults, res1, res2 };
